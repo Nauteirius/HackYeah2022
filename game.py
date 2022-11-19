929a3aac0bef2ce17add8066ec131937ec26da7c
Nauteirius <kamil.sordyl@student.uj.edu.pl>
2022-11-19 15:02:54 +0100

    Initial commit

--- game.py
+++ game.py
@@ -77,74 +77,7 @@ class Player():
 
 
 
-class Army():
-        def __init__(self):
-            self.attack
-            self.defence
-            self.initiative
-            self.hp
-            self.quantity
-            self.pos
 
-        def assault():
-            #wybierz okno przeciwnika ktorego ma zaatakowac
-            pass
-
-            
-        
-        def checker(object):
-            if(object pos)
-
-        
-
-class Infranty(Army):
-    pass
-
-
-
-class MuszkieterSzwedzki(Infranty):
-    def __init__(self):
-        self.attack=50
-        self.defence=50
-        self.initiative=10
-        self.hp=5
-        self.quantity=1
-        self.texture=""
-        self.name = "Muszkieter Szwedzki"
-
-    
-
-
-    def __init__(self,quan):
-        self.attack=50
-        self.defence=30
-        self.initiative=10
-        self.hp=5
-        self.quantity=quan
-        self.texture=""
-        self.name = "Muszkieter Szwedzki"
-
-class PiechotaWybraniecka(Infranty):
-    def __init__(self):
-        self.attack=30
-        self.defence=50
-        self.initiative=10
-        self.hp=5
-        self.quantity=1
-        self.texture=""
-        self.name = "Piechota Wybraniecka"
-
-    
-
-
-    def __init__(self,quan):
-        self.attack=30
-        self.defence=50
-        self.initiative=10
-        self.hp=5
-        self.quantity=quan
-        self.texture=""
-        self.name = "Piechota Wybraniecka"
 
 
 class Battle():
@@ -157,6 +90,6 @@ class Battle():
         
         queue.sort(key=lambda x: x.initiative, reverse=True)
 
-        
+
 
 
